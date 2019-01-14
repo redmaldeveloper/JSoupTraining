5102b437cf6d1ed64880aa6c3c8e45110945ab9f
Dustin Jordan <redmaldeveloper@gmail.com>
2019-01-13 17:19:00 -0900

    Day 2 - pulling link attribute and text

--- Day2.java
+++ Day2.java
@@ -14,6 +14,9 @@ public class Day2 {
     private static int counter = 0;
 
     public static void main(String[] args) throws IOException {
+        syntaxSelect();
+
+        /*
         String url = "http://yahoo.com/";
         Document document = Jsoup.connect(url).get();
 
@@ -24,6 +27,33 @@ public class Day2 {
             if(counter==10){
                 break TestRun;
             }
+        }
+        */
+
+    }
+
+
+    private static void syntaxSelect() {
+        String html = "<html>"
+                    + "<head><title>My Random Title</title></head>"
+                    + "<body>"
+                    + "<p>First Paragraph</p>"
+                    + "<p>Second Paragraph</p>"
+                    + "<div id='firstDiv'><a href='www.google.com'>Google</a>"
+                    + "<h3><a>REDMAL Header</a><h3>"
+                    + "</div>"
+                    + "<div id='secondDiv' class='header'><img name='google' src='google.png' />"
+                    + "<img name='yahoo' src='yahoo.jpg' />"
+                    + "</div>"
+                    + "</body>"
+                    + "</html>";
+
+        Document document = Jsoup.parse(html);
+        Elements links = document.select("a[href]");
+
+        for (Element link : links){
+            System.out.println("Href: " + link.attr("href"));
+            System.out.println("Text: " + link.text());
         }
     }
 }
