b9cf60f2b941f7384ffd8745e395968c73110d97
Dustin Jordan <trainerofdragons@gmail.com>
2019-01-13 16:48:38 -0900

    Day 2 - connect url, get elements by tag

--- Day2.java
+++ Day2.java
@@ -4,17 +4,26 @@
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 
 public class Day2 {
+    private static int counter = 0;
 
     public static void main(String[] args) throws IOException {
         String url = "http://yahoo.com/";
         Document document = Jsoup.connect(url).get();
 
-        System.out.println("Title: " + document.title());
-        System.out.println("Head: " + document.head());
+        Elements paragraphs = document.getElementsByTag("p");
+        TestRun: for (Element paragraph : paragraphs){
+            System.out.println((counter+1) + " " + paragraph);
+            counter += 1;
+            if(counter==10){
+                break TestRun;
+            }
+        }
     }
 }
