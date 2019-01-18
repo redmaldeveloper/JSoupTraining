0a11d780ed1e1ada8bf6f53f3121d8601dca8c19
Dustin Jordan <redmaldeveloper@gmail.com>
2019-01-17 21:31:05 -0900

    Day 4 - Extract HTML

--- Day4.java
+++ Day4.java
@@ -23,8 +23,13 @@ public class Day4 {
         //a w/ href
         Element link = document.select("a").first();
 
-        System.out.println("Outer html: " + link.outerHtml());
-        System.out.println("Inner html: " + link.html());
+        //head element w/ title
+        Element link2 = document.select("head").first();
 
+        System.out.println("Outer html: " + link.outerHtml());
+        System.out.println("Inner html: " + link.html() + "\n");
+
+        System.out.println("Outer html: " + link2.outerHtml());
+        System.out.println("Inner html: " + link2.html());
     }
 }
